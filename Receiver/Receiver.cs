06b4f975d51cb6bbb7e5fa310fa536374568c818
Henrik Kühl <henrikkyhl@hotmail.com>
2019-02-06 20:56:58 +0100

    Initial commit

---
+++ Receiver/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace Receiver
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Receive<TextMessage>("myQueue", message => HandleOrderReplyMessage(message));
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleOrderReplyMessage(TextMessage message)
+        {
+            //throw new Exception("Testing invalid message channel");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Received: " + message.Text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Sender/Sender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace Sender
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                var input = "";
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                {
+                    bus.Send<TextMessage>("myQueue", new TextMessage
+                    {
+                        Text = input
+                    });
+                }
+            }
+        }
+    }
+}
